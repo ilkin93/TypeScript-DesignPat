920a86b6cc80dd9b1881b9eac233d1d9efe1f421
ilkinismayilli <ilkin.ismayilli@bk.ru>
2020-11-11 09:54:44 +0400

    typescript

---
+++ readme.md
@@ -0,0 +1,1 @@
+Typescript Bubble sort and Design Pattern
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// bubble sort
+//type guard instance of
+
+import {Sorter} from './sorter'
+import {NumbersCollection} from "./numbersCollection";
+import {CharactersCollection} from "./charactersCollection";
+import {LinkedList} from "./linkedList";
+
+
+const number = new NumbersCollection([13, 2, -4, 0])
+number.sort();
+console.log(number.data)
+
+/**
+ * typeof for the primitive type, like string, number, symbol, boolean
+ *
+ *
+ *
+ *
+ * */
+
